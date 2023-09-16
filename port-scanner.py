8e98945ca71cc86235bdbe7eeec98c11807bb0bd
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-09-16 19:13:44 +0545

    bug fix and optimization

--- port-scanner.py
+++ port-scanner.py
@@ -1,45 +1,61 @@
 from socket import *
-import time,os,math
+import time
+import os
+import threading
+
 startTime = time.time()
 
-#### design
 def show_banner():
-   os.system("clear")
-   print(f'''
-                __
-|)             /__
-|  0 RT        __/C 4NN3R
-''')
+    os.system("clear")
+    print('''
+            __
+    |)      /__
+    |  0 RT  __/C 4NN3R
+    ''')
 
-def show_progress(progress_index,t_IP):
-    index = (progress_index/500)*100
-    if (index%2)==0:
+def show_progress(progress_index, t_IP):
+    index = (progress_index / 450) * 100
+    if int(index) % 2 == 0:
         show_banner()
-        print ('Starting scan on host: ', t_IP)
-        print(f"\n\n==========================\nScanning: {100-index}% left ")
-    else:
-      return 0
+        print('Starting scan on host:', t_IP)
+        print(f"\n\n==========================\nScanning: {100 - index:.1f}% left ")
 
-## config
-port_open = []
-# code
+def get_service(port):
+    try:
+        service = getservbyport(port, 'tcp')
+        return service
+    except OSError:
+        return 'Unknown Service'
+
+def scan_port(t_IP, port):
+    s = socket(AF_INET, SOCK_STREAM)
+    conn = s.connect_ex((t_IP, port))
+    if conn == 0:
+        service = get_service(port)
+        port_open.append((port, service))
+        print(f'Port {port}: OPEN ({service})')
+    else:
+        print(f'Port {port}: CLOSED')
+    s.close()
+
 if __name__ == '__main__':
-   show_banner()
-   target = input('Enter the host to be scanned: ')
-   t_IP = gethostbyname(target)
-   print ('Starting scan on host: ', t_IP)
-   for i in range(50, 500):
-      s = socket(AF_INET, SOCK_STREAM)
-      conn = s.connect_ex((t_IP, i))
-      if(conn == 0) :
-         port_open.append(str(i))
-         print ('Port %d: OPEN' % (i,))
-      else:
-         print(f"[PORT {i} FOUND INACTIVE ]")
-      s.close()
-      show_progress(i,t_IP)
-show_banner()
-print("Ports found open during scan: ")
-for port in port_open:
-   print(port_open(port))
-print('Time taken:', time.time() - startTime)
+    show_banner()
+    target = input('Enter the host to be scanned: ')
+    t_IP = gethostbyname(target)
+    print('Starting scan on host:', t_IP)
+    port_open = []
+    threads = []
+
+    for i in range(50, 500):
+        thread = threading.Thread(target=scan_port, args=(t_IP, i))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    show_banner()
+    print("Ports found open during scan: ")
+    for port, service in port_open:
+        print(f'Port {port}: {service}')
+    print('Time taken:', time.time() - startTime)
